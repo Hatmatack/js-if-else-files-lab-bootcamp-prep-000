e593bd16387ce24c5cbad84c9d6fcb9aeed1a1c9
Paul <pki@midmaine.com>
2018-01-29 18:38:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var name = "Joe"
 if (name ==="Susan") {
   console.log("The name is correct")
 }
-else { console.log ("Expected to be Susan")
+else {console.log ("Expected to be Susan")
 var height = "74"
 
 
